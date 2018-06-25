6ea09f287ff656e2d58dd4598071551d4b828c86
Rona Rago <ronalhyn.rago@gmail.com>
2018-06-25 23:00:31 +0800

    ROCKET IS LAUNCHING!!!

--- RocketLaunch/RocketLaunch/ViewController.swift
+++ RocketLaunch/RocketLaunch/ViewController.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
-
+import AVFoundation
 class ViewController: UIViewController {
-
+    
+    
+    //variable declaration
     @IBOutlet weak var darkbluebg: UIImageView!
     @IBOutlet weak var powerbtn: UIButton!
     @IBOutlet weak var cloudholder: UIView!
@@ -17,14 +19,39 @@ class ViewController: UIViewController {
     @IBOutlet weak var rocketlaunchlbl: UILabel!
     @IBOutlet weak var onlbl: UILabel!
     
+    
+    var player: AVAudioPlayer!
+    
+    //Starting point of the code
     override func viewDidLoad() {
         super.viewDidLoad()
+        //grabbing the wav file
+        let path = Bundle.main.path(forResource: "hustle-on", ofType: "wav")!
+        let url = URL(fileURLWithPath: path)
+        do{
+            player = try AVAudioPlayer(contentsOf: url)
+            player.prepareToPlay()
+        }
+        catch let error as NSError{
+            print(error.description)
+        }
     }
-
+    
     @IBAction func powerbtnpressed(_ sender: Any) {
         cloudholder.isHidden = false
         darkbluebg.isHidden = true
         powerbtn.isHidden = true
+        
+        player.play()
+        
+        //Animation
+        UIView.animate(withDuration: 2.4, animations: {
+            self.rocket.frame = CGRect(x: 0, y: 50, width: 414, height: 532)
+        }) { (finished) in
+            self.rocketlaunchlbl.isHidden = false
+            self.onlbl.isHidden = false
+            
+        }
     }
     
 
